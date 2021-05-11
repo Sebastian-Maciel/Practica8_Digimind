e0d31b9d6a4657a83e456dc7e988bc4f9f6dbf58
Sebastián García Maciel <sebastian-maciel@outlook.com>
2021-05-11 17:05:27 -0500

    Arreglo de error al mostrar las tareas

--- app/src/main/java/garciamaciel/sebastian/mydigimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/garciamaciel/sebastian/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -1,29 +1,94 @@
 package garciamaciel.sebastian.mydigimind.ui.dashboard
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import garciamaciel.sebastian.mydigimind.R
+import garciamaciel.sebastian.mydigimind.ui.Task
+import garciamaciel.sebastian.mydigimind.ui.home.HomeFragment
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
+        val btn_time: Button = root.findViewById(R.id.btn_time)
+
+        btn_time.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(
+                root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), true
+            ).show()
+        }
+
+        var btn_save = root.findViewById(R.id.btn_save) as Button
+        var et_titulo = root.findViewById(R.id.et_task) as EditText
+        var checkMonday = root.findViewById(R.id.checkMonday) as CheckBox
+        var checkTuesday = root.findViewById(R.id.checkTuesday) as CheckBox
+        var checkWednesday = root.findViewById(R.id.checkWednesday) as CheckBox
+        var checkThursday = root.findViewById(R.id.checkThursday) as CheckBox
+        var checkFriday = root.findViewById(R.id.checkFriday) as CheckBox
+        var checkSaturday = root.findViewById(R.id.checkSaturday) as CheckBox
+        var checkSunday = root.findViewById(R.id.checkSunday) as CheckBox
+
+
+        btn_save.setOnClickListener {
+            var title = et_titulo.text.toString()
+            var time = btn_time.text.toString()
+            var days = ArrayList<String>()
+
+
+            if (time.equals("Set time")) {
+                Toast.makeText(root.context, "You must set the time", Toast.LENGTH_SHORT).show()
+            } else {
+                if (checkMonday.isChecked)
+                    days.add("Monday")
+                if (checkTuesday.isChecked)
+                    days.add("Tuesday")
+                if (checkWednesday.isChecked)
+                    days.add("Wednesday")
+                if (checkThursday.isChecked)
+                    days.add("Thursday")
+                if (checkFriday.isChecked)
+                    days.add("Friday")
+                if (checkSaturday.isChecked)
+                    days.add("Saturday")
+                if (checkSunday.isChecked)
+                    days.add("Sunday")
+
+                var task = Task(title, days, time)
+
+                HomeFragment.tasks.add(task)
+                Toast.makeText(root.context, "New task added", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
         return root
     }
 }
--- app/src/main/java/garciamaciel/sebastian/mydigimind/ui/home/HomeFragment.kt
+++ app/src/main/java/garciamaciel/sebastian/mydigimind/ui/home/HomeFragment.kt
@@ -1,31 +1,97 @@
 package garciamaciel.sebastian.mydigimind.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import garciamaciel.sebastian.mydigimind.R
+import garciamaciel.sebastian.mydigimind.ui.Task
 
 class HomeFragment : Fragment() {
 
+
+    private var adaptador: AdaptadorTareas? = null
     private lateinit var homeViewModel: HomeViewModel
 
+    companion object {
+        var tasks = ArrayList<Task>()
+        var first = true
+    }
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        if (first) {
+            fillTask()
+            first = false
+        }
+
+
+        adaptador = AdaptadorTareas(root.context, tasks)
+        var gridView: GridView = root.findViewById(R.id.gridView)
+
+        gridView.adapter = adaptador
+
         return root
+    }
+
+    fun fillTask() {
+        tasks.add(Task("Practice 1", arrayListOf("Tuesday"), "17:30"))
+        tasks.add(Task("Practice 2", arrayListOf("Monday", "Sunday"), "17:40"))
+        tasks.add(Task("Practice 3", arrayListOf("Wednesday"), "14:00"))
+        tasks.add(Task("Practice 4", arrayListOf("Saturday"), "11:00"))
+        tasks.add(Task("Practice 5", arrayListOf("Friday"), "13:00"))
+        tasks.add(Task("Practice 6", arrayListOf("thursday"), "10:40"))
+        tasks.add(Task("Practice 7", arrayListOf("Monday"), "12:00"))
+    }
+
+    private class AdaptadorTareas : BaseAdapter {
+        var tasks = ArrayList<Task>()
+        var contexto: Context? = null
+
+        constructor(contexto: Context, tasks: java.util.ArrayList<Task>) {
+            this.contexto = contexto
+            this.tasks = tasks
+        }
+
+        override fun getCount(): Int {
+            return tasks.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return tasks[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var task = tasks[position]
+            var inflater = LayoutInflater.from(contexto)
+            var vista = inflater.inflate(R.layout.task_view, null)
+
+            var tv_title: TextView = vista.findViewById(R.id.tv_title)
+            var tv_time: TextView = vista.findViewById(R.id.tv_time)
+            var tv_days: TextView = vista.findViewById(R.id.tv_days)
+
+            tv_title.setText(task.title)
+            tv_time.setText(task.time)
+            tv_days.setText(task.days.toString())
+
+            return vista
+        }
     }
 }
